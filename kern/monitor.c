afc1040ff70470e0fc9e151730f64cc818edcf3f
Pengcheng Tang <pctang@caicloud.io>
2019-02-18 00:59:27 +0800

    Finish lab1

--- kern/monitor.c
+++ kern/monitor.c
@@ -24,6 +24,7 @@ struct Command {
 static struct Command commands[] = {
 	{ "help", "Display this list of commands", mon_help },
 	{ "kerninfo", "Display information about the kernel", mon_kerninfo },
+	{ "backtrace", "Display backtrace info", mon_backtrace },
 };
 
 /***** Implementations of basic kernel monitor commands *****/
@@ -58,6 +59,24 @@ int
 mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 {
 	// Your code here.
+	uint32_t ebp, eip, fn_offset, *ptr_ebp;
+	struct Eipdebuginfo info;
+
+	ebp = read_ebp();
+	cprintf("Stack backtrace:\n");
+	while (ebp) {
+		ptr_ebp = (uint32_t *)ebp;
+		cprintf("  ebp %x  eip %x  args %08x %08x %08x %08x %08x\n",
+			ebp, ptr_ebp[1], ptr_ebp[2], ptr_ebp[3], ptr_ebp[4], ptr_ebp[5], ptr_ebp[6]);
+
+		// get debug info from stabs section
+		debuginfo_eip(ptr_ebp[1], &info);
+		fn_offset = ptr_ebp[1] - info.eip_fn_addr;
+
+		cprintf("\t%s:%d: %.*s+%d\n", info.eip_file, info.eip_line,
+			info.eip_fn_namelen, info.eip_fn_name, fn_offset);
+		ebp = *ptr_ebp;
+	}
 	return 0;
 }
 
